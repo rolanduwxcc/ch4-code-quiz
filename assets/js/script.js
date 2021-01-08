293cc318044e8b2f66276269982433bb56bae9b2
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-08 01:09:40 -0600

    updated question objects and timer counts down

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,81 @@
 //---------------------------------------------------------Variables
-var questionAnswerList = {
-    qCount: 5,
-
-    Q1: "Commonly used data types DO NOT include:",
-    A1: ["strings", "booleans", "alerts-1", "numbers"],
-    
-    Q2: "The condition in an if/else statement is enclosed with __________.",
-    A2: ["quotes", "curly brackets", "parenthesis-1", "square brackets"],
-
-    Q3: "Arrays in JavaScript can be used to store ____________.",
-    A3: ["numbers and strings", "other arrays", "booleans", "all of the above-1"],
-
-    Q4: "String values must be enclosed within ______________ when being assigned to avariables.",
-    A4: ["commas", "curly brackets", "quotes-1", "parenthesis"],
-
-    Q5: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    A5: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-};
-
-var qaPage = document.querySelector(".question-wrapper");
-
-
-
-
-
-
+var quizData = [
+    // { 
+    //     Q: "Welcome", 
+    //     A: [ "Try to answer the following code-related questions withing the time limit. \nKeep in mind that incorrect answers will penalize your score/time \nby ten seconds!"],
+    //     C: "1"
+    // },
+    { 
+        Q: "Commonly used data types DO NOT include:", 
+        A: ["strings", "booleans", "alerts-1", "numbers"]
+    },
+    {
+        Q: "The condition in an if/else statement is enclosed with __________.",
+        A: ["quotes", "curly brackets", "parenthesis-1", "square brackets"],
+    },
+    {
+        Q: "Arrays in JavaScript can be used to store ____________.",
+        A: ["numbers and strings", "other arrays", "booleans", "all of the above-1"],
+    },
+    {
+        Q: "String values must be enclosed within ______________ when being assigned to avariables.",
+        A: ["commas", "curly brackets", "quotes-1", "parenthesis"],
+    },
+    {
+        Q: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        A: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    }
+];
+var timerEl = document.getElementById("quiz-timer");
+var questionWrapEl = document.querySelector(".question-wrapper");
+var timeLeft = 60;
 
 //---------------------------------------------------------Functions
+function scoreTimer() {
+    
+
+    var timeInterval = setInterval(function() {
+        if (timeLeft > 0) {
+            timerEl.textContent = "Timer: " + timeLeft;
+            timeLeft--;
+        } 
+        else {
+            timerEl.textContent = "Timer: DONE";
+            clearInterval(timeInterval);
+        }
+    },1000);
+}
+
 var startPage = function() {
-    var questionEl = document.createElement("h2"); 
-    questionEl.textContent = questionAnswerList.Q1;
-    qaPage.appendChild(questionEl);
+    //set the timer
+    //loop over questions
+    scoreTimer();
 
-    var answersListEl = document.createElement("ol");
-    for (let i = 0; i < questionAnswerList.A1.length; i++) {
-        const element = questionAnswerList.A1[i];
-        var answerEl = document.createElement("li");
-        answerEl.innerHTML = "<li>" + element + "</li>";
-        answersListEl.appendChild(answerEl);
-    }    
+    console.log(quizData.length);
+    for (var i = 0; i < quizData.length; i++) {
+        var element = quizData[i];
+        var questionEl = document.createElement("h2");
+        var answerChoicesEl = document.createElement("ul");
+        questionEl.textContent = element.Q;
 
-    qaPage.appendChild(answersListEl);
+        var answerList = element.A;        
+        for (let j = 0; j < answerList.length; j++) {
+            const answer = answerList[j];
+            var answerEl = document.createElement("li");
+            answerEl.textContent =  answer;
+            answerEl.innerHTML = "<button id='1'>" + answer + "</button>";
+            answerChoicesEl.appendChild(answerEl);
+        }
+        questionWrapEl.appendChild(questionEl);
+        questionWrapEl.appendChild(answerChoicesEl);
+    }
+
+    
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
 
 //---------------------------------------------------------Listeners
 
 
-
-
-
-
-
-
-
-////---------------------------------------------------------Calls
+//---------------------------------------------------------Calls
 startPage();
