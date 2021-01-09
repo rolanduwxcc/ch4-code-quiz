39290c0adf4f957a2999abc27ed49f065aac94ea
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-09 00:20:33 -0600

    removed for loop making functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,5 @@
 //---------------------------------------------------------Variables
 var quizData = [
-    // { 
-    //     Q: "Welcome", 
-    //     A: [ "Try to answer the following code-related questions withing the time limit. \nKeep in mind that incorrect answers will penalize your score/time \nby ten seconds!"],
-    //     C: "1"
-    // },
     { 
         Q: "Commonly used data types DO NOT include:", 
         A: ["strings", "booleans", "alerts-1", "numbers"]
@@ -46,36 +41,81 @@ function scoreTimer() {
     },1000);
 }
 
+var askQuestion = function(i) {
+    var element = quizData[i];
+    var questionEl = document.createElement("h2");
+    var answerChoicesEl = document.createElement("ul");
+    questionEl.textContent = element.Q;
+
+    var answerList = element.A;        
+    for (let j = 0; j < answerList.length; j++) {
+        const answer = answerList[j];
+        var answerEl = document.createElement("li");
+        answerEl.textContent =  answer;
+        answerEl.innerHTML = "<button class='answer-btn' id='1'>" + answer + "</button>";
+        answerChoicesEl.appendChild(answerEl);
+    }
+    questionWrapEl.appendChild(questionEl);
+    questionWrapEl.appendChild(answerChoicesEl);
+
+}
+
 var startPage = function() {
     //set the timer
     //loop over questions
+    var quizLength = quizData.length; 
+    var questionNumber = 0;
     scoreTimer();
+    askQuestion(questionNumber);
 
-    console.log(quizData.length);
-    for (var i = 0; i < quizData.length; i++) {
-        var element = quizData[i];
-        var questionEl = document.createElement("h2");
-        var answerChoicesEl = document.createElement("ul");
-        questionEl.textContent = element.Q;
+    var welcomeTitleEl = document.createElement("h2");
+    welcomeTitleEl.textContent = "Coding Quiz Challenge";
+    var welcomeInstructionsEl = document.createElement("p");
+    welcomeInstructionsEl.textContent = "Try to answer the following code-related questions withing the time limit. \nKeep in mind that incorrect answers will penalize your score/time \nby ten seconds!";
+    var startQuizBtn = document.createElement("button");
+    startQuizBtn.textContent = "Start Quiz";
 
-        var answerList = element.A;        
-        for (let j = 0; j < answerList.length; j++) {
-            const answer = answerList[j];
-            var answerEl = document.createElement("li");
-            answerEl.textContent =  answer;
-            answerEl.innerHTML = "<button id='1'>" + answer + "</button>";
-            answerChoicesEl.appendChild(answerEl);
-        }
-        questionWrapEl.appendChild(questionEl);
-        questionWrapEl.appendChild(answerChoicesEl);
-    }
+    questionWrapEl.appendChild(welcomeTitleEl);
+    questionWrapEl.appendChild(welcomeInstructionsEl);
+    questionWrapEl.appendChild(startQuizBtn);
+
+    // for (var i = 0; i < quizData.length; i++) {
+    //     var element = quizData[i];
+    //     var questionEl = document.createElement("h2");
+    //     var answerChoicesEl = document.createElement("ul");
+    //     questionEl.textContent = element.Q;
+
+    //     var answerList = element.A;        
+    //     for (let j = 0; j < answerList.length; j++) {
+    //         const answer = answerList[j];
+    //         var answerEl = document.createElement("li");
+    //         answerEl.textContent =  answer;
+    //         answerEl.innerHTML = "<button id='1'>" + answer + "</button>";
+    //         answerChoicesEl.appendChild(answerEl);
+    //     }
+    //     questionWrapEl.appendChild(questionEl);
+    //     questionWrapEl.appendChild(answerChoicesEl);
+    //}
 
     
 
 };
 
-//---------------------------------------------------------Listeners
+var answerButtonHandler = function(event) {
+    // console.log(event.target); //logs what object triggered event
 
+    if (event.target.matches(".answer-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+    else if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+//---------------------------------------------------------Listeners
+questionWrapEl.addEventListener("click",answerButtonHandler);
 
 //---------------------------------------------------------Calls
 startPage();
